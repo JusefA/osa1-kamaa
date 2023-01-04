bb4e4c7df11f3844eb8929ee43a76cfc74173a20
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 15:56:27 +0200

    Add project files.

---
+++ osa1 kamaa/osa1_kamaa.py
@@ -0,0 +1,9 @@
+c = int(input("Anna luku: "))
+if c < 1000:
+    print("Luku on pienempi kuin 1000")
+elif c < 100:
+    print("Luku on pienempi kuin 100")
+elif c < 10:
+    print("Luku on pienempi kuin 10")
+else:
+    print("Kiitos!")
